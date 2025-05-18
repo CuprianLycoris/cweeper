18c58f5a379a04f6fdd233fc49257bca80cf365a
Cuprian <cuprianlycoris@gmail.com>
2025-05-18 18:13:21 +0300

    Init a window and draw a field

--- src/main.c
+++ src/main.c
@@ -12,14 +12,33 @@ typedef struct {
 } Tile;
 
 #define GRID_SIZE 16
+#define TILE_SIZE 30
+#define WIDTH GRID_SIZE * TILE_SIZE
+#define HEIGHT GRID_SIZE * TILE_SIZE
 
 int main(void) {
-    InitWindow(800, 600, "gay sex");
+    InitWindow(WIDTH, HEIGHT, "cweeper");
     Tile grid[GRID_SIZE][GRID_SIZE] = {0};
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RED);
+        ClearBackground(BLACK);
+
+        for (int i = 0; i < GRID_SIZE; i++){
+            for (int j = 0; j < GRID_SIZE; j++)
+            {
+                grid[10][6].state = TILE_OPEN;
+                if (grid[i][j].state == TILE_CLOSE)
+                {
+                DrawRectangle(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE, WHITE);
+                }
+                else if (grid[i][j].state == TILE_OPEN)
+                {
+                    DrawRectangle(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE, ORANGE);
+                }
+            }
+        }
+        
         
         EndDrawing();
     }
